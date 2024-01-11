72412d4f29a55be103657093b64a24528d7a78db
prathiswaran-gr <prathis2003@gmail.com>
2024-01-11 11:17:30 +0530

    initial push

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  const [isCreateAreaClicked, setAreaClicked] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+  function onClickEvent() {
+    setAreaClicked(true);
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+          onClick={onClickEvent}
+        />
+        {isCreateAreaClicked && (
+          <textarea
+            name="content"
+            onChange={handleChange}
+            value={note.content}
+            placeholder="Take a note..."
+            rows="3"
+          />
+        )}
+        <Zoom in={isCreateAreaClicked}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import CreateIcon from '@mui/icons-material/Create';
+
+function Header() {
+  return (
+    <header>
+      <h1><CreateIcon />Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
